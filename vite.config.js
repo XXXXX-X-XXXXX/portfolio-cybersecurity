97f8f867395afd1417cc940e1f06a31b3f83f9e9
DESBANS Martin <desbans@MacBook-Air-de-DESBANS.local>
2026-02-18 18:18:48 +0100

    bug page blanche

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/porfolio-cybersecurity/', // nom EXACT du repo
+  base: '/portfolio-cybersecurity/', // nom EXACT du repo
   plugins: [react()],
 });
